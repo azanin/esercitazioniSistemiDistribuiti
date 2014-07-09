9012b481652a42e1aecf9ede407ad054059ad7d9
Alessandro Zanin <alessandro.zanin@studio.unibo.it>
2014-07-09 10:38:28 +0200

    added security.xml SecurityPolicy.java

---
+++ esercitazioneSpring/ProgettoSistemiDistribuitiLS_SpringHibernate_MySolution/src/it/unibo/sdls/sampleproject/security/SecurityPolicy.java
@@ -0,0 +1,16 @@
+package it.unibo.sdls.sampleproject.security;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class SecurityPolicy implements MethodInterceptor {
+
+	public Object invoke(MethodInvocation arg0) throws Throwable {
+		
+		//qui va la logica per implementare la sicurezza
+		
+		
+		return null;
+	}
+
+}
